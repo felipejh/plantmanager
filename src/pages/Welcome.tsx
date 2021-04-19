af5cdcf77321f4b2f9bd7b98eca13e04ad4e00da
Felipe Hoffmann <felipe.hoffmann@nl.com.br>
2021-04-19 16:13:31 -0300

    feat: Final aula 01

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,16 +1,56 @@
-﻿import React from 'react';
-import { View, Text, Image } from 'react-native';
+﻿import React from "react";
+import {
+  SafeAreaView,
+  Text,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
+import colors from "../../styles/colors";
+import { Button } from "../components/Button";
 
-import wateringImg from '../assets/watering.png'
+import wateringImg from "../assets/watering.png";
 
 export function Welcome() {
   return (
-    <View>
-      <Text>
-        Gerencie suas plantas de forma fácil
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>
+        Gerencie {"\n"} suas plantas {"\n"} de forma fácil
       </Text>
 
-      <Image source={wateringImg} />
-    </View>
-  )
+      <Image style={styles.image} source={wateringImg} />
+
+      <Text style={styles.subtitle}>
+        Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
+        sempre que precisar.
+      </Text>
+
+      <Button title='>' />
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  image: {
+    width: 292,
+    height: 284,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: colors.heading,
+    marginTop: 38,
+  },
+  subtitle: {
+    textAlign: "center",
+    fontSize: 18,
+    paddingHorizontal: 20,
+    color: colors.heading,
+  },
+});
