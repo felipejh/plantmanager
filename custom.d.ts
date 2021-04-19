8e08fa97c8272af51a9048441060fc75e3e5cd3d
Felipe Hoffmann <felipe.hoffmann@nl.com.br>
2021-04-19 15:17:26 -0300

    Initial commit

---
+++ custom.d.ts
@@ -0,0 +1,4 @@
+﻿declare module '*.png' {
+  const content: any;
+  export default content;
+}
---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,16 @@
+﻿import React from 'react';
+import { View, Text, Image } from 'react-native';
+
+import wateringImg from '../assets/watering.png'
+
+export function Welcome() {
+  return (
+    <View>
+      <Text>
+        Gerencie suas plantas de forma fácil
+      </Text>
+
+      <Image source={wateringImg} />
+    </View>
+  )
+}
